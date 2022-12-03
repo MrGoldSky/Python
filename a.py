cca77fa5641ca8ac9c637e40a23a36b9b9021971
Ilia Rosseev <ilia19042006@gmail.com>
2022-12-03 18:11:18 +0300

    a

--- a.py
+++ a.py
@@ -1,21 +1,11 @@
-import sys
-
-pokyp = {}
-
-a = list(map(str.strip, sys.stdin))
-
-for i in a:
-    b = i.split()
-    user, p, count = b[0], b[1], int(b[2])
-    if pokyp.get(user) == None:
-        pokyp[user] = {}
-    pokyp[user][p] = pokyp[user].get(p, 0) + count
-
-for i in pokyp:
-    pokyp[i] = dict(sorted(pokyp[i].items(), key=lambda item: item[0]))
-
-for i in pokyp.keys():
-    print(f"{i}:")
-    pk = pokyp[i]
-    for j in pk.keys():
-        print(f"{j} {pk[j]}")
+import numpy as np
+ 
+a = input()
+a = np.array(list(a))
+ 
+_, cnt = np.unique(a, return_counts=True)
+p = cnt/np.sum(cnt)
+ 
+H = -np.sum(p * np.log2(p))
+ 
+print(round(H, 2))
